4fed48bf77c1dae73232cdff6d2bd87dbb94e2b1
Sudha Agrahari <sudhaagrahari79@gmail.com>
2025-05-29 15:40:39 +0530

    added implementation for Multiply.java

--- Multiply.java
+++ Multiply.java
@@ -1,9 +1,6 @@
 public class Multiply {
     public static void main(String[] args) {
-         int a = 4;
-    int b = 5;
-    System.out.println("the product is : " + (a*b));
-    
+        
         
     }
    
